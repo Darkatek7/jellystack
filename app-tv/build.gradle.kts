20aca4ce360240c80fcb6dddaf9f7b89cc984bc8
Darkatek7 <heeltobias@gmail.com>
2026-08-24 18:48:05 +0200

    chore(tv): prepare beta 6

--- app-tv/build.gradle.kts
+++ app-tv/build.gradle.kts
@@ -50,8 +50,8 @@ android {
         applicationId = "app.jellystack.mobile"
         minSdk = 24
         targetSdk = 36
-        versionCode = 22
-        versionName = "0.16.0-tv-beta.4"
+        versionCode = 24
+        versionName = "0.16.0-tv-beta.6"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
